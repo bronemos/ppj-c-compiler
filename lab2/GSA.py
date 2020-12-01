036bff2a14e03d4929ae29e4131efd1b74d3a6a9
Jan Grgić <jan.grgic@fer.hr>
2020-12-01 16:23:37 +0100

    GSA working

--- lab2/GSA.py
+++ lab2/GSA.py
@@ -2,6 +2,10 @@ import re
 import sys
 from copy import deepcopy
 from collections import defaultdict
+import time
+import pickle as serializer
+
+start_time = time.time()
 
 braces_regex = re.compile('\<(.+)\>')
 
@@ -83,7 +87,7 @@ for k, v in begins.items():
 
 for k, v in begins.items():
     begins[k] = {x[0] for x in v}
-print(begins)
+# print(begins)
 
 sequence_end = {'#'}
 last_point_index = -1  # easier to check for reduction later
@@ -116,7 +120,6 @@ while len(nonterminals_to_process) > 0:
                 if key[0] == production[index][0]:  # symbol in grammar dict is equeal to nonterminal after point
                     new_state_index = 0
                     if grammar_dict[key][0] == ('$', False):
-                        print(key)
                         new_state_index = -1
                     if last:
                         after_set = frozenset(current_nonterminal[1])
@@ -140,20 +143,24 @@ while len(nonterminals_to_process) > 0:
                         after_set = frozenset(after_set)
                         enka_dict[enka_dict_key].add((key, after_set, new_state_index))
 
-                    if (key, after_set) not in processed_nonterminals and (key, after_set) not in nonterminals_to_process:
+                    if (key, after_set) not in processed_nonterminals and (
+                            key, after_set) not in nonterminals_to_process:
                         nonterminals_to_process.append((key, after_set))
 
-for k, v in enka_dict.items():
-    print(f'{k} : {v}')
+# for k, v in enka_dict.items():
+#     print(f'{k} : {v}')
 # +++++++++++++++++++++ do ovdje sve radi
 
 # find all enka_states
+# brojac = 0
 enka_states = set()
 for k, v in enka_dict.items():
     enka_states.add(k[:3])
     enka_states = enka_states | v
+    # brojac += len(v)
 
-print(len(enka_states))
+# print(f'Broj stanja u eNKA: {len(enka_states)}')
+# print(f'Broj prijelaza u eNKA: {brojac}')
 
 # create NKA from ENKA
 # nadji epsilon okruzenja svakog stanja
@@ -172,20 +179,20 @@ for state in enka_states:
     else:
         epsilon_okruzenja[state] = {state}
 
-print(len(epsilon_okruzenja))  # OK
+# print(len(epsilon_okruzenja))  # OK
 
-nka_dict = defaultdict(set)
+helper_nka_dict = defaultdict(set)
 for k, v in enka_dict.items():
     if k[3] == ('$', False):
         continue
     else:
         for state in v:
-            nka_dict[k] |= epsilon_okruzenja[state]
+            helper_nka_dict[k] |= epsilon_okruzenja[state]
 
-print(len(nka_dict))
-dka_dict = {}
-# example frozenset({produkcije...}), ('A', True) -> frozenset({produkcije...})
-# ===== dka_dict[(frozenset({produkcije...}), ('A', True))] = frozenset({produkcije...})
+# print(len(helper_nka_dict))
+nka_dict = defaultdict(set)
+# example frozenset({produkcije...}), ('A', True) -> {frozenset({produkcije...}), frozenset()}
+# ===== nka_dict[(frozenset({produkcije...}), ('A', True))] = {frozenset({produkcije...}), fs()}
 state_key_dict = {0: frozenset(epsilon_okruzenja[((first_state, 0), frozenset({'#'}), 0)])}
 state_key_dict_reversed = {frozenset(epsilon_okruzenja[((first_state, 0), frozenset({'#'}), 0)]): 0}
 states_to_process = [0]
@@ -196,7 +203,7 @@ while len(states_to_process) > 0:
     processed_states.append(state_num)
     find_transitions_for = state_key_dict[state_num]
     for state in find_transitions_for:
-        for k, v in nka_dict.items():
+        for k, v in helper_nka_dict.items():
             if k[:3] == state:
                 if not frozenset(v) in state_key_dict.values():
                     states_num += 1
@@ -204,34 +211,127 @@ while len(states_to_process) > 0:
                     state_key_dict_reversed[frozenset(v)] = states_num
                     if states_num not in states_to_process and states_num not in processed_states:
                         states_to_process.append(states_num)
-                dka_dict[(state_num, k[3])] = state_key_dict_reversed[frozenset(v)]
+                nka_dict[(state_num, k[3])].add(state_key_dict_reversed[frozenset(v)])
 
-for k, v in dka_dict.items():
-    print(k, ':', v)
+# brojac = 0
+# for k, v in nka_dict.items():
+#     brojac += len(v)
+# print(k, ':', v)
+# print(f'Broj stanja u NKA: {len(state_key_dict)}')
+# print(f'Broj prijelaza u NKA: {brojac}')
+# for k, v in state_key_dict.items():
+#     print(f'{k} : {v}')
+# print('--' * 10)
 
-# make transitions
-# for k, v in dka_states_dict.items():
-#     for production in v:
-#         for k1, v1 in enka_dict.items():
-#             if k1[:3] == production and k1[3:][0][0] != '$':
-#                 for k2, v2 in dka_states_dict.items():
-#                     if v1 <= v2:
-#                         dka_dict[(k, k1[3:][0])] = k2
-#                         break
+state_num = 0
+dka_dict = {}  # 0, a -> 2
+dka_state_key_dict = {}  # 0 -> frozenset()
+dka_state_key_dict_reversed = {}  # frozenset() -> 0
 
-# print(len(dka_dict))
-# create tables action and new_state
-# Pomakni/Reduciraj proturjeˇcje izgradeni generator treba razrijeˇsiti u korist akcije Pomakni. Reduciraj/Reduciraj
-# proturječje potrebno je razrijeˇsiti u korist one akcije koja reducira produkciju zadanu ranije u Ulaznoj Datoteci
 
-# actions = {}  # (0, ('a', False)) -> (1, 'move' or 'reduce', production ('A', 1) -> only needed for reduce)
-# new_state = {}  # (0, ('A', True)) -> 4
+# Finally create DKA from NKA
+def addDkaState(snka):
+    global state_num
+    global dka_dict
+    global dka_state_key_dict
+    global dka_state_key_dict_reversed
+    #     ako DKA već sadrži stanje koje odgovara skupu:
+    #         vrati to stanje;
+    if dka_state_key_dict_reversed.get(frozenset(snka)) is not None:
+        return dka_state_key_dict_reversed.get(frozenset(snka))
+
+    #     prijelazi = nova mapa(znak -> skup stanja NKA);
+    transitions = defaultdict(set)  # a -> {1, 3, 5, 7}
+
+    #     za svako stanje s u snka:
+    #         za svaki prijelaz (znak k prelazi u skup stanja NKA p) od s:
+    #             dodaj sva stanja iz p u prijelazi[k];
+    for state in snka:
+        for k, v in nka_dict.items():
+            if state == k[0]:
+                transitions[k[1]] = transitions[k[1]] | v
+
+    #     U DKA stvori novo stanje sdka koje odgovara skupu snka;
+    key = state_num
+    dka_state_key_dict[key] = frozenset(snka)
+    dka_state_key_dict_reversed[frozenset(snka)] = key
+    state_num += 1
+    #
+    #     za svaki prijelaz (znak z prelazi u skup stanja NKA p) iz prijelazi:
+    #         U DKA dodaj prijelaz iz sdka u dohvatiStanje(p) za znak z;
+    for k, v in transitions.items():
+        dka_dict[(key, k)] = addDkaState(v)
+
+    #     Vrati sdka;
+    return key
+
+
+addDkaState({0})
+
 # for k, v in dka_dict.items():
-#     if not k[1][1]:  # transition is for nonterminal, add to action table
-#         for nonterminal in nonterminals:
-#             if k[1][0] == nonterminal:
-#                 # provjeri treba li izvsiti akciju pomakni, ako ne pogledaj moze li se reducrati. Uzmi prvu akciju reduciraj
-#                 pass
-#
-#     else:  # add to new_state table
-#         pass
+#     print(f'{k} : {v}')
+# for k, v in dka_state_key_dict.items():
+#     print(f'{k} : {v}')
+# print(f'Broj stanja u DKA: {len(dka_state_key_dict)}')
+# print(f'Broj prijelaza u DKA: {len(dka_dict)}')
+
+# check productions associated with each state in DKA
+reduction_for_dka_state = {}
+# only reductions are needed here
+for k, v in dka_state_key_dict.items():  # za sva stanja u dka
+    reductions = set()
+    for nka_state in v:  # za sva nka stanja koja se nalaze u dka stanju
+        for production in state_key_dict[nka_state]:  # za svaku produkciju koju sadrzi nka stanje
+            if production[2] == -1:  # tocka je na kraju
+                reductions.add(production)  # dodaj redukciju u set
+    if len(reductions) > 0:
+        reduction_for_dka_state[k] = reductions  # dodaj set redukcija za staje
+
+# print(reduction_for_dka_state)
+# create tables action and new_state
+# Pomakni/Reduciraj proturjecje izgradeni generator treba razrijesiti u korist akcije Pomakni. Reduciraj/Reduciraj
+# proturječje potrebno je razrijesiti u korist one akcije koja reducira produkciju zadanu ranije u Ulaznoj Datoteci
+# analizatoru treba proslijediti action, new_state i grammar_dict
+actions = {}  # (0, ('a', False)) -> (1, 'move' == True or 'reduce', production ('A', 1) -> only needed for reduce)
+new_state = {}  # (0, ('A', True)) -> 4
+for k in dka_state_key_dict.keys():
+    # provjeri treba li izvsiti akciju pomakni, ako ne pogledaj moze li se reducrati. Uzmi prvu akciju reduciraj
+    for nonterminal in nonterminals:
+        if dka_dict.get((k, (nonterminal, True))) is not None:
+            new_state[(k, nonterminal)] = dka_dict.get((k, (nonterminal, True)))
+
+    # provjeri treba li izvsiti akciju pomakni, ako ne pogledaj moze li se reducrati. Uzmi prvu akciju reduciraj
+    for terminal in terminals:
+        reduction = None
+        if reduction_for_dka_state.get(k) is not None:
+            reduction = None
+            for reduction_for_state in frozenset(reduction_for_dka_state.get(k)):
+                if terminal in reduction_for_state[1]:
+                    if reduction is None:
+                        reduction = reduction_for_state[0]
+                    else:
+                        print(f'reduciraj - reduciraj proturječje stanje: {k}, znak: {terminal}', file=sys.stderr, end='')
+                        if reduction_for_state[0][1] < reduction[1]:
+                            reduction = reduction_for_state[0]
+        if dka_dict.get((k, (terminal, False))) is not None:
+            actions[(k, terminal)] = (True, dka_dict.get((k, (terminal, False))))
+            if reduction is not None:
+                print(f'pomakni - reduciraj proturječje stanje: {k}, znak: {terminal}, pomakni: {dka_dict.get((k, (terminal, False)))}, reduciraj {reduction}', file=sys.stderr, end='')
+        elif reduction is not None:
+            actions[(k, terminal)] = (False, (reduction[0], True), grammar_dict.get(reduction))
+
+with open('lab2/analizator/new_state.txt', 'wb') as f:
+    serializer.dump(new_state, f)
+
+with open('lab2/analizator/actions.txt', 'wb') as f:
+    serializer.dump(actions, f)
+# print('----------NEW_STATE----------')
+# for k, v in new_state.items():
+#     print(f'{k} : {v}')
+# print(new_state)
+# print('----------ACTIONS----------')
+# for k, v in actions.items():
+#     print(f'{k} : {v}')
+# print(actions)
+
+# print(f'Time: {time.time() -  start_time}')
