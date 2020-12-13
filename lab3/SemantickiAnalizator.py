e06da7757b958fa49bbf3111b358774cc2dafe51
bronemos <spieglb@gmail.com>
2020-12-13 22:32:38 +0100

    add fill_tree function

--- lab3/SemantickiAnalizator.py
+++ lab3/SemantickiAnalizator.py
@@ -3,7 +3,13 @@ from __future__ import annotations
 import sys
 import re
 
-pattern = re.compile(r'^(\s*)(.*)$')
+
+class Node:
+
+    def __init__(self, data: str, parent: Node = None):
+        self.parent = parent
+        self.data = data
+        self.children = []
 
 
 def dfs_print(root: Node, prefix=''):
@@ -14,33 +20,31 @@ def dfs_print(root: Node, prefix=''):
         dfs_print(child, prefix)
 
 
-class Node:
+def fill_tree(parent: Node, tree_list: list):
+    previous_space_count = -1
+    pattern = re.compile(r'^(\s*)(.*)$')
 
-    def __init__(self, data: str, parent: Node = None):
-        self.parent = parent
-        self.data = data
-        self.children = []
+    for node in tree_list[1:]:
+        match = pattern.match(node)
+        space_count = len(match.group(1))
+        node_data = match.group(2)
+        if space_count > previous_space_count:
+            parent.children.append(Node(node_data, parent))
+            parent = parent.children[-1]
+        else:
+            while space_count <= previous_space_count:
+                parent = parent.parent
+                previous_space_count -= 1
+            parent.children.append(Node(node_data, parent))
+            parent = parent.children[-1]
+
+        previous_space_count = space_count
 
 
 tree_list = sys.stdin.read().splitlines()
 
-previous_space_count = -1
 root = Node(tree_list[0])
-parent = root
 
-for node in tree_list[1:]:
-    match = pattern.match(node)
-    space_count = len(match.group(1))
-    node_data = match.group(2)
-    if space_count > previous_space_count:
-        parent.children.append(Node(node_data, parent))
-        parent = parent.children[-1]
-    else:
-        while space_count <= previous_space_count:
-            parent = parent.parent
-            previous_space_count -= 1
-        parent.children.append(Node(node_data, parent))
-        parent = parent.children[-1]
+fill_tree(root, tree_list)
 
-    previous_space_count = space_count
-
+dfs_print(root)
